ea0db4091bc3cf6570ee4d814529f4b5046effb7
Septiana Yoga <septianayoga30@gmail.com>
2021-01-14 16:03:52 +0700

    updating most affected card

--- src/components/MostAffected.js
+++ src/components/MostAffected.js
@@ -3,25 +3,25 @@ import styles from './MostAffected.module.css'
 import Color from './Color.module.css'
 
 const MostAffected = () => (
-  <div className="most-affected-container">
+  <div>
     <h3 className={styles.most_affected_title}>Most Affected Country</h3>
-    <div className="most-affected-container">
+    <div>
       <div className={styles.most_affected_card}>
         <h3 className={styles.affected_card_title}><b>Afganistan</b> from continent <b>Asia</b></h3>
         <div className={styles.most_affected_data}>
-          <div className="affected-confirmed">
+          <div>
             <p className={styles.data_number}>99999</p>
             <p className={styles.data_title}>Confirmed</p>
           </div>
-          <div className="affected-recovered">
+          <div>
             <p className={[styles.data_number, Color.text_green].join(" ")}>99999</p>
             <p className={styles.data_title}>Recovered</p>
           </div>
-          <div className="affected-deaths">
+          <div>
             <p className={[styles.data_number, Color.text_red].join(" ")}>99999</p>
             <p className={styles.data_title}>Deaths</p>
           </div>
-          <div className="affected-population">
+          <div>
             <p className={[styles.data_number, Color.text_blue].join(" ")}>99999</p>
             <p className={styles.data_title}>Population</p>
           </div>
