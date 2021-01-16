63c18024a3220825dbd41b3259cdad1e2dd83a9d
Septiana Yoga <septianayoga30@gmail.com>
2021-01-17 00:18:10 +0700

    fix bug and update countries card

--- src/components/ContinentCardLoader.js
+++ src/components/ContinentCardLoader.js
@@ -3,76 +3,24 @@ import React from 'react'
 import styles from './ContinentsData.module.css'
 import Color from './Color.module.css'
 
-/*
-  {
-    Asia: {
-      totalCountries: 10,
-      totalConfirmed: 10,
-      totalDeaths: 10,
-      totalRecovered: 10
-    }
-  }
-*/
-
 const dataCounter = (data) => {
-  data = data.CovidData.data
-  let dataObject = {}
+  let dataPush = []
+  let testPush = {}
+
+  // loop data json
   for (let i in data) {
-    // counting total country in continent
-    if (data[i].All.continent !== undefined) {
-      if (!dataObject.hasOwnProperty(`${data[i].All.continent}`)) {
-        dataObject[data[i].All.continent] = {}
-        dataObject[data[i].All.continent]['totalCountry'] = 1
-      } else {
-        dataObject[data[i].All.continent]['totalCountry']++
+    // loop untuk memeriksa apakah data sudah ada
+    for (let j = 0; j < dataPush.length; j++) {
+      if(dataPush[j].hasOwnProperty(data[i].All.continent) === false){
+        testPush.continentName = data[i].All.continent
+        testPush.totalConfirmed = data[i].All.confirmed
       }
     }
-
-    // counting for total confirmed
-    if (data[i].All.confirmed !== undefined && data[i].All.continent !== undefined) {
-      if (!dataObject[data[i].All.continent].hasOwnProperty('totalConfirmed')) {
-        dataObject[data[i].All.continent]['totalConfirmed'] = data[i].All.confirmed
-      } else {
-        dataObject[data[i].All.continent]['totalConfirmed'] += data[i].All.confirmed
-      }
-    }
-
-    // counting for total deaths
-    if (data[i].All.deaths !== undefined && data[i].All.continent !== undefined) {
-      if (!dataObject[data[i].All.continent].hasOwnProperty('totalDeaths')) {
-        dataObject[data[i].All.continent]['totalDeaths'] = data[i].All.deaths
-      } else {
-        dataObject[data[i].All.continent]['totalDeaths'] += data[i].All.deaths
-      }
-    }
-
-    // counting for total recovered
-    if (data[i].All.recovered !== undefined && data[i].All.continent !== undefined) {
-      if (!dataObject[data[i].All.continent].hasOwnProperty('totalRecovered')) {
-        dataObject[data[i].All.continent]['totalRecovered'] = data[i].All.recovered
-      } else {
-        dataObject[data[i].All.continent]['totalRecovered'] += data[i].All.recovered
-      }
-    }
-
   }
-
-  return dataObject
-}
-
-const CardGenerator = (data) => {
-  let totalCountry = dataCounter(data)
-  
-  console.log(totalCountry)
 }
 
 const ContinentCardLoader = (props) => (
-
-  
   <div className={styles.continent_card}>
-    {
-      CardGenerator(props)
-    }
     <h3 className={styles.continent_card_header}>Asia</h3>
     <div className="continent-total-country">
       <p className={styles.total_country_header}>Total Country</p>
@@ -84,7 +32,5 @@ const ContinentCardLoader = (props) => (
       <p className={styles.continent_data_deceased}><b>9999</b> People <b className={Color.text_white}>Deceased</b></p>
     </div>
   </div>
-
 )
-
 export default ContinentCardLoader
--- src/components/ContinentsData.js
+++ src/components/ContinentsData.js
@@ -1,17 +1,106 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ContinentCardLoader from './ContinentCardLoader'
 
 import styles from './ContinentsData.module.css'
+import Color from './Color.module.css'
 
-const ContinentData = (props) => (
-  <div className={styles.continents_container}>
-    <h3 className={styles.continents_title}>Data by continents</h3>
-    <div className={styles.continents_card_container}>
+class ContinentData extends Component {
 
-      <ContinentCardLoader CovidData={props} />      
+  dataCounter = (data) => {
+    let dataObject = []
+    
+    // looping all data
+    for (let i in data) {
+      let dataExists = 'no'
+      // check if the dataObject has data[i] name
+      
+      if (data[i].All.continent !== undefined) { // check if this is not country data
+        dataObject.forEach(function(continent) { // loop for the data
+            if(
+              continent.hasOwnProperty("continentName") &&
+              continent.continentName === data[i].All.continent
+            ) {
+              dataExists = 'yes'
+            }
+        })
 
-    </div>
-  </div>
-)
+        // if data exists, add the particular data
+        // if no create new data.
+
+        if (dataExists === 'no') {
+          dataObject.push({
+            "continentName": data[i].All.continent,
+            "totalConfirmed": data[i].All.confirmed,
+            "totalDeaths": data[i].All.deaths,
+            "totalRecovered": data[i].All.recovered,
+            "totalCountries": 1
+          })
+        } 
+        if (dataExists === 'yes') {
+          dataObject.forEach(continent => {
+            if (continent.continentName === data[i].All.continent) {
+              continent.totalConfirmed += data[i].All.confirmed
+              continent.totalDeaths += data[i].All.deaths
+              continent.totalRecovered += data[i].All.recovered
+              continent.totalCountries += 1
+            }
+          });
+
+          dataExists = 'no'
+        }
+
+      }
+
+    }
+    
+    // console.log('data object', dataObject)
+    return dataObject
+  }
+
+  changeNumber = (num) => {
+    if (num > 1000000) {
+      num = Math.sign(num)*((Math.abs(num)/1000000).toFixed(1)) + 'M'
+    } else if (num > 1000) {
+      num = Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'K'
+    }
+
+    return num
+  }
+
+  render() {
+    let dataRender = this.dataCounter(this.props.data)
+    dataRender.forEach(continent => {
+        continent.totalConfirmed = this.changeNumber(continent.totalConfirmed)
+        continent.totalDeaths = this.changeNumber(continent.totalDeaths)
+        continent.totalRecovered = this.changeNumber(continent.totalConfirmed)
+    })
+
+    return (
+      <div className={styles.continents_container}>
+        <h3 className={styles.continents_title}>Data by continents</h3>
+        <div className={styles.continents_card_container}>
+
+          {
+            dataRender.map((continent, index) => (
+              <div key={index} className={styles.continent_card}> {/* card start from here */}
+                <h3 className={styles.continent_card_header}>{continent.continentName}</h3>
+                <div>
+                  <p className={styles.total_country_header}>Total Country</p>
+                  <p className={styles.total_country_subheader}>{continent.totalCountries}</p>
+                </div>
+                <div>
+                  <p className={styles.continent_data_confirmed}><b>{continent.totalConfirmed}</b> People <b className={Color.text_black}>Confirmed</b></p>
+                  <p className={styles.continent_data_recovered}><b>{continent.totalRecovered}</b> People <b className={Color.text_green}>Recovered</b></p>
+                  <p className={styles.continent_data_deceased}><b>{continent.totalDeaths}</b> People <b className={Color.text_white}>Deceased</b></p>
+                </div>
+              </div>
+            ))
+          }
+
+        </div>
+      </div>
+    )
+  }
+}
 
 export default ContinentData
